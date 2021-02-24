baccf432d7a07b723e02daf4596102b188c18ca3
Zailer43 <63565983+Zailer43@users.noreply.github.com>
2021-02-24 16:29:49 -0300

    mefaltounarchivo._.XD

---
+++ src/main/java/fzmm/zailer/me/mixin/ItemFrameEntityRendererMixin.java
@@ -0,0 +1,24 @@
+package fzmm.zailer.me.mixin;
+
+import fzmm.zailer.me.config.FzmmConfig;
+import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.decoration.ItemFrameEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(net.minecraft.client.render.entity.ItemFrameEntityRenderer.class)
+public abstract class ItemFrameEntityRendererMixin {
+
+    @Inject(method = "render", at = @At("HEAD"), cancellable = true)
+    public void render(ItemFrameEntity itemFrameEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo info) {
+        FzmmConfig config = AutoConfig.getConfigHolder(FzmmConfig.class).getConfig();
+
+        if (config.general.forceInvisibleItemFrame) {
+            itemFrameEntity.setInvisible(true);
+        }
+    }
+}
