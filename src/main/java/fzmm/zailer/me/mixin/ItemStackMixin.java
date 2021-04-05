0168e490d0e91f3f8bc02f75a8ab1aac17e8d7c4
Zailer43 <63565983+Zailer43@users.noreply.github.com>
2021-04-04 22:38:00 -0300

    Ahora al tener descripciones avanzadas activadas puedes ver los tags

---
+++ src/main/java/fzmm/zailer/me/mixin/ItemStackMixin.java
@@ -0,0 +1,57 @@
+package fzmm.zailer.me.mixin;
+
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Mixin(ItemStack.class)
+public abstract class ItemStackMixin {
+
+    @Shadow @Nullable public abstract CompoundTag getTag();
+
+    @Inject(method = "getTooltip", at = @At("RETURN"), cancellable = true)
+    public void getTooltip(PlayerEntity player, TooltipContext context, CallbackInfoReturnable<List<Text>> cir) {
+        if (this.getTag() != null && context.isAdvanced()) {
+
+            List<String> stringList = new ArrayList<>();
+            final String[] string = {Formatting.DARK_GRAY + "Tag's: "};
+            Set<String> tags = this.getTag().getKeys();
+            AtomicInteger i = new AtomicInteger(0);
+            int tagssize = tags.size();
+
+            tags.forEach(element -> {
+                string[0] += element;
+                i.getAndIncrement();
+                if (i.get() != tagssize) string[0] += ", ";
+                if (i.get() % 3 == 0 && i.get() != tagssize) {
+                    stringList.add(string[0]);
+                    string[0] = Formatting.DARK_GRAY + "";
+                } else if (i.get() == tagssize) {
+                    stringList.add(string[0]);
+                }
+
+            });
+
+            List<Text> list = cir.getReturnValue();
+            for (int a = 0; a != stringList.size(); a++)
+                list.add(new LiteralText(stringList.get(a)));
+
+            cir.setReturnValue(list);
+        }
+    }
+}
