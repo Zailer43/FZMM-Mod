88916dffe2e8451d27daa1252fc11106395ad0a3
Zailer43 <63565983+Zailer43@users.noreply.github.com>
2021-05-05 14:19:04 -0300

    Renombra PlayerMixin.java a ClientPlayerEntityMixin.java

--- src/main/java/fzmm/zailer/me/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/fzmm/zailer/me/mixin/ClientPlayerEntityMixin.java
@@ -16,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayerEntity.class)
-public abstract class PlayerMixin {
+public abstract class ClientPlayerEntityMixin {
 
     @Shadow @Final public ClientPlayNetworkHandler networkHandler;
 
