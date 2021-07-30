453fb55a3d8ac5cb19b415801eb7881b90411b41
Zailer43 <minecraftpelusa@gmail.com>
2021-07-30 14:07:17 -0300

    Arregla copiar la invisibilidad de armor stands

--- src/main/java/fzmm/zailer/me/mixin/ArmorStandEntityMixin.java
+++ src/main/java/fzmm/zailer/me/mixin/ArmorStandEntityMixin.java
@@ -6,9 +6,8 @@ import net.minecraft.entity.decoration.ArmorStandEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
 import net.minecraft.nbt.NbtList;
-import net.minecraft.util.collection.DefaultedList;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
@@ -16,26 +15,7 @@ import org.spongepowered.asm.mixin.Shadow;
 @Mixin(ArmorStandEntity.class)
 public abstract class ArmorStandEntityMixin {
 
-	@Shadow
-	public abstract boolean isSmall();
-
-	@Shadow
-	public abstract boolean shouldShowArms();
-
-	@Shadow
-	public abstract boolean shouldHideBasePlate();
-
-	@Shadow
-	private NbtCompound poseToNbt() {
-		return null;
-	}
-
-	@Shadow
-	@Final
-	private DefaultedList<ItemStack> armorItems;
-	@Shadow
-	@Final
-	private DefaultedList<ItemStack> heldItems;
+	@Shadow public abstract void writeCustomDataToNbt(NbtCompound nbt);
 
 	/**
 	 * @reason Get the armor stand item with NBT if Ctrl is pressed
@@ -46,27 +26,14 @@ public abstract class ArmorStandEntityMixin {
 		ItemStack stack = Items.ARMOR_STAND.getDefaultStack();
 		if (Screen.hasControlDown()) {
 			NbtCompound entityTag = new NbtCompound();
-			NbtList armorNbt = new NbtList(),
-				handNbt = new NbtList();
-			DefaultedList<ItemStack> armor = this.armorItems,
-				hand = this.heldItems;
 			NbtCompound display = stack.getOrCreateSubTag("display");
-			NbtList lore = display.getList("Lore", 8);
+			NbtList lore = display.getList("Lore", NbtElement.STRING_TYPE);
 
 			lore.add(FzmmUtils.generateLoreMessage("(EntityTag)"));
 			display.put("Lore", lore);
 
-			FzmmUtils.defaultListToNbtList(armor, armorNbt);
-			FzmmUtils.defaultListToNbtList(hand, handNbt);
-
-			entityTag.put("Pose", this.poseToNbt());
-			entityTag.put("ArmorItems", armorNbt);
-			entityTag.put("HandItems", handNbt);
-			entityTag.putBoolean("Small", this.isSmall());
-			entityTag.putBoolean("ShowArms", this.shouldShowArms());
-			//entityTag.putBoolean("Invisible", this.isInvisible()); //TODO: Problemas de compatibilidad con otros mods
-			//entityTag.putBoolean("NoGravity", this.hasNoGravity());
-			entityTag.putBoolean("NoBasePlate", this.shouldHideBasePlate());
+			this.writeCustomDataToNbt(entityTag);
+			// FIXME: DisabledSlots and NoGravity do not work
 
 			stack.putSubTag("EntityTag", entityTag);
 			stack.putSubTag("display", display);
