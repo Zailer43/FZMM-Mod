5e9d593cde17422b1e0f53cfba31c1d76e2af4d1
Zailer43 <minecraftpelusa@gmail.com>
2021-08-13 00:06:58 -0300

    Item frames * Añade que al presionar ctrl+copiar en un item frame te da el item frame con el item, invisibilidad, rotación y fixed * Arregla que al desactivar la opción para que los item frames sean invisibles los que ya lo eran no dejan de serlo

---
+++ src/main/java/fzmm/zailer/me/mixin/ItemFrameEntityMixin.java
@@ -0,0 +1,40 @@
+package fzmm.zailer.me.mixin;
+
+import fzmm.zailer.me.utils.FzmmUtils;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.entity.decoration.ItemFrameEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ItemFrameEntity.class)
+public abstract class ItemFrameEntityMixin {
+
+    @Shadow protected abstract ItemStack getAsItemStack();
+
+    @Shadow public abstract void writeCustomDataToNbt(NbtCompound nbt);
+
+    @Inject(method = "getPickBlockStack", at = @At(value = "HEAD"), cancellable = true)
+    public void getPickBlockStack(CallbackInfoReturnable<ItemStack> cir) {
+        // item_frame{EntityTag:{Item:{id:"minecraft:tnt",Count:1b,tag:{test:1b}},Invisible:1b,Fixed:1b}}
+
+        if (Screen.hasControlDown()) {
+            ItemStack stack = this.getAsItemStack();
+            NbtCompound entityTag = new NbtCompound();
+
+            this.writeCustomDataToNbt(entityTag);
+
+            entityTag.remove("TileX");
+            entityTag.remove("TileY");
+            entityTag.remove("TileZ");
+
+            stack.putSubTag("EntityTag", entityTag);
+            stack.putSubTag("display", FzmmUtils.generateLoreMessage("(EntityTag)"));
+            cir.setReturnValue(stack);
+        }
+    }
+}
--- src/main/java/fzmm/zailer/me/mixin/ItemFrameEntityRendererMixin.java
+++ src/main/java/fzmm/zailer/me/mixin/ItemFrameEntityRendererMixin.java
@@ -3,22 +3,29 @@ package fzmm.zailer.me.mixin;
 import fzmm.zailer.me.config.FzmmConfig;
 import me.shedaniel.autoconfig.AutoConfig;
 import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.ItemFrameEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.decoration.ItemFrameEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(net.minecraft.client.render.entity.ItemFrameEntityRenderer.class)
-public abstract class ItemFrameEntityRendererMixin {
+@Mixin(ItemFrameEntityRenderer.class)
+public class ItemFrameEntityRendererMixin<T extends ItemFrameEntity> {
+
+    private boolean hasStack;
 
     @Inject(method = "render", at = @At("HEAD"))
-    public void render(ItemFrameEntity itemFrameEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo info) {
+    private void render(T itemFrameEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
+        hasStack = itemFrameEntity.getHeldItemStack().isEmpty();
+    }
+
+    @ModifyVariable(method = "render", at = @At("STORE"))
+    private boolean disableItemFrameFrameRendering(boolean bl) {
         FzmmConfig config = AutoConfig.getConfigHolder(FzmmConfig.class).getConfig();
 
-        if (config.general.forceInvisibleItemFrame && !itemFrameEntity.getHeldItemStack().isEmpty()) {
-            itemFrameEntity.setInvisible(true);
-        }
+        return (config.general.forceInvisibleItemFrame && !hasStack) || bl;
     }
 }
