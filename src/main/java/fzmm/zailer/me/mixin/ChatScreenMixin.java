5aa581ffab70d5a764e8aa85e3d05a82111884f6
Zailer43 <minecraftpelusa@gmail.com>
2022-01-07 16:47:03 -0300

    Small improvements when using fzmm command * Fix bug when using the chat history that does not respect the history * increase chat limit to 200k

--- src/main/java/fzmm/zailer/me/mixin/ChatScreenMixin.java
+++ src/main/java/fzmm/zailer/me/mixin/ChatScreenMixin.java
@@ -6,6 +6,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ChatScreen.class)
@@ -15,8 +16,22 @@ public class ChatScreenMixin {
 
     @Inject(method = "onChatFieldUpdate", at = @At("HEAD"))
     public void onChatFieldUpdate(String chatText, CallbackInfo ci) {
+        this.setFzmmCommandMaxLength(chatText);
+    }
 
-        if (chatText.startsWith("/fzmm ")) this.chatField.setMaxLength(30000);
-        else this.chatField.setMaxLength(256);
+    @Redirect(method = "setChatFromHistory", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/TextFieldWidget;setText(Ljava/lang/String;)V"))
+    protected void setText(TextFieldWidget textField, String text) {
+        this.setFzmmCommandMaxLength(text);
+        textField.setText(text);
+    }
+
+    private void setFzmmCommandMaxLength(String message) {
+        if (message.startsWith("/fzmm "))
+            this.chatField.setMaxLength(200000);
+        else {
+            if (this.chatField.getCursor() > 256)
+                this.chatField.setCursor(Math.min(256, this.chatField.getText().length()));
+            this.chatField.setMaxLength(256);
+        }
     }
 }
