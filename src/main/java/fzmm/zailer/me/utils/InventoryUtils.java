bb77c8aedab087e32d4f74838396a395749d9cef
Zailer43 <minecraftpelusa@gmail.com>
2022-01-29 00:14:36 -0300

    Update InventoryUtils

--- src/main/java/fzmm/zailer/me/utils/InventoryUtils.java
+++ src/main/java/fzmm/zailer/me/utils/InventoryUtils.java
@@ -1,10 +1,79 @@
 package fzmm.zailer.me.utils;
 
+import net.minecraft.block.entity.ShulkerBoxBlockEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
 import net.minecraft.nbt.NbtList;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Style;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class InventoryUtils {
+
+    private final ItemStack container;
+    private final List<ItemStack> items;
+
+    public InventoryUtils(ItemStack container) {
+        this.container = container;
+        this.items = getItemsFromContainer(this.container);
+    }
+
+    public InventoryUtils addItem(List<ItemStack> items) {
+        this.items.addAll(items);
+        return this;
+    }
+
+    public InventoryUtils setNameStyleToItems(Style style) {
+        for (ItemStack stack : this.items) {
+            String name = new DisplayUtils(stack).getName();
+            if (name == null)
+                continue;
+            MutableText nameText;
+            try {
+                nameText = Text.Serializer.fromJson(name);
+                if (nameText == null)
+                    nameText = new LiteralText(name);
+
+                nameText.setStyle(style);
+                stack.setCustomName(nameText);
+            } catch (Exception ignored) {
+            }
+        }
+        return this;
+    }
+
+    public InventoryUtils addLoreToItems(Item itemToApply, String lore, int color) {
+        for (ItemStack stack : this.items) {
+            if (stack.getItem() == itemToApply) {
+                NbtCompound tag = new DisplayUtils(stack).addLore(lore, color).getNbt();
+                stack.setNbt(tag);
+            }
+        }
+        return this;
+    }
+
+    public ItemStack get() {
+        NbtCompound blockEntityTag = new NbtCompound();
+        NbtList items = new NbtList();
+
+        int itemsLength = this.items.size();
+        for (int i = 0; i != itemsLength; i++) {
+            NbtCompound slotTag = getSlotTag(this.items.get(i), i);
+            items.add(slotTag);
+        }
+
+        blockEntityTag.put(ShulkerBoxBlockEntity.ITEMS_KEY, items);
+        this.container.setSubNbt(TagsConstant.BLOCK_ENTITY, blockEntityTag);
+        return this.container;
+    }
 
     public static void addSlot(NbtList slotList, ItemStack stack, int slot) {
         slotList.add(getSlotTag(stack, slot));
@@ -12,19 +81,60 @@ public class InventoryUtils {
 
     public static NbtCompound getSlotTag(ItemStack stack, int slot) {
         NbtCompound slotTag = stackToTag(stack);
-        slotTag.putByte("Slot", (byte) slot);
+        slotTag.putByte(TagsConstant.INVENTORY_SLOT, (byte) slot);
         return slotTag;
     }
 
     public static NbtCompound stackToTag(ItemStack stack) {
         NbtCompound tag = new NbtCompound();
 
-        tag.putString("id", stack.getItem().toString());
-        tag.putByte("Count", (byte) stack.getCount());
+        tag.putString(TagsConstant.INVENTORY_ID, stack.getItem().toString());
+        tag.putByte(TagsConstant.INVENTORY_COUNT, (byte) stack.getCount());
 
         if (stack.hasNbt())
-            tag.put("tag", stack.getNbt());
+            tag.put(TagsConstant.INVENTORY_TAG, stack.getNbt());
 
         return tag;
+    }
+
+    public static List<ItemStack> getItemsFromContainer(ItemStack container) {
+        List<ItemStack> items = new ArrayList<>();
+
+        NbtCompound blockEntityTag = container.getOrCreateSubNbt(TagsConstant.BLOCK_ENTITY);
+        if (blockEntityTag.contains(ShulkerBoxBlockEntity.ITEMS_KEY, NbtElement.LIST_TYPE)) {
+            NbtList itemsTag = blockEntityTag.getList(ShulkerBoxBlockEntity.ITEMS_KEY, NbtElement.COMPOUND_TYPE);
+
+            for (NbtElement itemTag : itemsTag) {
+                if (itemTag instanceof NbtCompound itemCompound) {
+                    try {
+
+                        if (!itemCompound.contains(TagsConstant.INVENTORY_ID, NbtElement.STRING_TYPE))
+                            continue;
+                        String idString = itemCompound.getString(TagsConstant.INVENTORY_ID);
+                        Item item = Registry.ITEM.get(new Identifier(idString));
+
+                        if (!itemCompound.contains(TagsConstant.INVENTORY_COUNT, NbtElement.BYTE_TYPE))
+                            continue;
+                        byte count = itemCompound.getByte(TagsConstant.INVENTORY_COUNT);
+
+                        if (!itemCompound.contains(TagsConstant.INVENTORY_SLOT, NbtElement.BYTE_TYPE))
+                            continue;
+
+                        ItemStack stack = new ItemStack(item);
+                        stack.setCount(count);
+
+                        if (itemCompound.contains(TagsConstant.INVENTORY_TAG, NbtElement.COMPOUND_TYPE)) {
+                            NbtCompound tag = itemCompound.getCompound(TagsConstant.INVENTORY_TAG);
+                            stack.setNbt(tag);
+                        }
+
+                        items.add(stack);
+                    } catch (Exception ignored) {
+                    }
+                }
+            }
+        }
+
+        return items;
     }
 }
--- src/main/java/fzmm/zailer/me/utils/TagsConstant.java
+++ src/main/java/fzmm/zailer/me/utils/TagsConstant.java
@@ -2,4 +2,10 @@ package fzmm.zailer.me.utils;
 
 public class TagsConstant {
     public static final String BLOCK_ENTITY = "BlockEntityTag";
+    public static final String INVENTORY_COUNT = "Count";
+    public static final String INVENTORY_ID = "id";
+    public static final String INVENTORY_TAG = "tag";
+    public static final String INVENTORY_SLOT = "Slot";
+
+
 }
