2d4bb905abce5eaa22df39b76c4d5be76d96dad1
Zailer43 <minecraftpelusa@gmail.com>
2022-03-28 13:41:31 -0300

    Remove copy block states (alt + copy key) * I recommend using Pick Block Pro

--- src/main/java/fzmm/zailer/me/mixin/MinecraftClientMixin.java
+++ src/main/java/fzmm/zailer/me/mixin/MinecraftClientMixin.java
@@ -1,22 +1,14 @@
 package fzmm.zailer.me.mixin;
 
-import fzmm.zailer.me.client.PickItem;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
 
 	@Shadow
 	public ClientPlayerEntity player;
-
-	@Redirect(method = "handleInputEvents", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;doItemPick()V"))
-	private void doItemPick(MinecraftClient client) {
-		PickItem.doItemPick(client);
-	}
 
 }
