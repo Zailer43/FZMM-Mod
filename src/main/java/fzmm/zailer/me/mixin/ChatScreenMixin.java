2d1cd90adcbd66a4111226a3f507ab04ff4abe1c
Zailer43 <minecraftpelusa@gmail.com>
2023-01-17 16:23:52 -0300

    Prevents chat normalization when using /fzmm * This fixes the removal of the double spaces when using the command

--- src/main/java/fzmm/zailer/me/mixin/ChatScreenMixin.java
+++ src/main/java/fzmm/zailer/me/mixin/ChatScreenMixin.java
@@ -8,19 +8,20 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ChatScreen.class)
-public class ChatScreenMixin {
+public abstract class ChatScreenMixin {
 
     @Shadow protected TextFieldWidget chatField;
 
     @Inject(method = "onChatFieldUpdate", at = @At("HEAD"))
-    public void onChatFieldUpdate(String chatText, CallbackInfo ci) {
+    private void onChatFieldUpdate(String chatText, CallbackInfo ci) {
         this.setFzmmCommandMaxLength(chatText);
     }
 
     @Redirect(method = "setChatFromHistory", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/TextFieldWidget;setText(Ljava/lang/String;)V"))
-    protected void setText(TextFieldWidget textField, String text) {
+    private void setText(TextFieldWidget textField, String text) {
         this.setFzmmCommandMaxLength(text);
         textField.setText(text);
     }
@@ -33,5 +34,11 @@ public class ChatScreenMixin {
                 this.chatField.setCursor(Math.min(256, this.chatField.getText().length()));
             this.chatField.setMaxLength(256);
         }
+    }
+
+    @Inject(method = "normalize", at = @At(value = "HEAD"), cancellable = true)
+    private void avoidNormalizeWithFzmmCommand(String str, CallbackInfoReturnable<String> cir) {
+        if (str.startsWith("/fzmm "))
+            cir.setReturnValue(str);
     }
 }
