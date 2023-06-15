8120f8b70e60fe51f3f783710eb1d6281c08b3a8
Zailer43 <minecraftpelusa@gmail.com>
2023-06-15 03:28:32 -0300

    Update mod version to 0.2.3

--- src/main/java/fzmm/zailer/me/utils/FzmmWikiConstants.java
+++ src/main/java/fzmm/zailer/me/utils/FzmmWikiConstants.java
@@ -4,7 +4,7 @@ public class FzmmWikiConstants {
     public static final String CURRENT_BRANCH = "1.19.4/dev";
     public static final String BASE_LINK = "https://github.com/Zailer43/FZMM-Mod/blob/" + CURRENT_BRANCH + "/docs/en/wiki/";
 
-    public static final String HEAD_GENERATOR_LAST_CHANGE_VERSION = "0.2.0";
+    public static final String HEAD_GENERATOR_LAST_CHANGE_VERSION = "0.2.3";
     public static final String HEAD_GENERATOR_WIKI_LINK = BASE_LINK + "head_generator/" + HEAD_GENERATOR_LAST_CHANGE_VERSION + "_head_generator.md";
 
     public static final String PLAYER_STATUE_LAST_CHANGE_VERSION = "0.1.2";
