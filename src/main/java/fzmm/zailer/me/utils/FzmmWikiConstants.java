b73848642c0880526d1843860799051749576163
Zailer43 <minecraftpelusa@gmail.com>
2024-01-20 22:41:01 -0300

    Update docs and images

--- src/main/java/fzmm/zailer/me/utils/FzmmWikiConstants.java
+++ src/main/java/fzmm/zailer/me/utils/FzmmWikiConstants.java
@@ -7,7 +7,7 @@ public class FzmmWikiConstants {
     public static final String HEAD_GENERATOR_LAST_CHANGE_VERSION = "0.2.3";
     public static final String HEAD_GENERATOR_WIKI_LINK = BASE_LINK + "head_generator/" + HEAD_GENERATOR_LAST_CHANGE_VERSION + "_head_generator.md";
 
-    public static final String PLAYER_STATUE_LAST_CHANGE_VERSION = "0.1.2";
+    public static final String PLAYER_STATUE_LAST_CHANGE_VERSION = "0.2.9";
     public static final String PLAYER_STATUE_WIKI_LINK = BASE_LINK + "player_statue/" + PLAYER_STATUE_LAST_CHANGE_VERSION + "_player_statue.md";
 
 
