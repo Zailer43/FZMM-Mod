88ec351a1e4d816f8798b45c11718f7e6ce50780
Zailer43 <zailer.g2jku@slmails.com>
2025-01-02 03:30:04 -0300

    Fix crash in head generator when updating a compound * This was due to the textures of the preview being updated outside the main thread * Backport the code from branch 1.21.4
    Signed-off-by: Zailer43 <zailer.g2jku@slmails.com>

--- src/main/java/fzmm/zailer/me/client/gui/components/image/source/ScreenshotSource.java
+++ src/main/java/fzmm/zailer/me/client/gui/components/image/source/ScreenshotSource.java
@@ -174,9 +174,8 @@ public class ScreenshotSource implements IInteractiveImageLoader {
 
                 FzmmUtils.setScreen(this.previousScreen);
                 this.previousScreen = null;
+                this.setImage(image);
             });
-
-            this.setImage(image);
         });
     }
 
