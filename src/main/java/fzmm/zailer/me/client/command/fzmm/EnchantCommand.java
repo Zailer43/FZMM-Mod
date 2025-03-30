d80a6f8527cb39b4f9220f0f0c9ccbee5343d66e
Zailer43 <zailer.g2jku@slmails.com>
2025-03-30 20:39:49 -0300

    Fix issue where `/fzmm enchant` does not allow lowering the level of an enchantment * If an enchantment that was already added is added again, as since some versions enchantments cannot be repeated, the issue prevented lowering its level, only increasing it

--- src/main/java/fzmm/zailer/me/client/command/fzmm/EnchantCommand.java
+++ src/main/java/fzmm/zailer/me/client/command/fzmm/EnchantCommand.java
@@ -31,11 +31,10 @@ public class EnchantCommand implements ISubCommand {
     @Override
     public LiteralCommandNode<FabricClientCommandSource> getBaseCommand(CommandRegistryAccess registryAccess, LiteralArgumentBuilder<FabricClientCommandSource> builder) {
         return builder.then(ClientCommandManager.argument("enchantment", RegistryEntryReferenceArgumentType.registryEntry(registryAccess, RegistryKeys.ENCHANTMENT)).executes(ctx -> {
-
             @SuppressWarnings("unchecked")
             Enchantment enchant = ((RegistryEntry.Reference<Enchantment>) ctx.getArgument("enchantment", RegistryEntry.Reference.class)).value();
 
-            addEnchant(enchant, (short) 1);
+            this.setEnchant(enchant, (short) 1);
             return 1;
 
         }).then(ClientCommandManager.argument("level", IntegerArgumentType.integer(0, 255)).executes(ctx -> {
@@ -44,18 +43,18 @@ public class EnchantCommand implements ISubCommand {
             Enchantment enchant = ((RegistryEntry.Reference<Enchantment>) ctx.getArgument("enchantment", RegistryEntry.Reference.class)).value();
             int level = ctx.getArgument("level", int.class);
 
-            addEnchant(enchant, (short) level);
+            this.setEnchant(enchant, (short) level);
             return 1;
         }))).build();
     }
 
-    private static void addEnchant(Enchantment enchant, short level) {
+    private void setEnchant(Enchantment enchant, short level) {
         //{Enchantments:[{message:"minecraft:aqua_affinity",lvl:1s}]}
         ItemStack stack = ItemUtils.from(Hand.MAIN_HAND);
 
         stack.apply(DataComponentTypes.ENCHANTMENTS, ItemEnchantmentsComponent.DEFAULT, component -> {
             ItemEnchantmentsComponent.Builder builder = new ItemEnchantmentsComponent.Builder(component);
-            builder.add(enchant, level);
+            builder.set(enchant, level);
             return builder.build();
         });
 
