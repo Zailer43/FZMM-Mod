248fe269012711eef6d6e6d80b20db3609ee5e6b
Zailer43 <zailer.g2jku@slmails.com>
2025-03-30 20:52:08 -0300

    Add lazy-load to item tooltips to improve performance when loading categories in the Head Gallery * Generating a tooltip from a stack can be expensive, which is noticeable in the Head Gallery, change this so that it is only generated when hovering the cursor over it

--- src/main/java/fzmm/zailer/me/client/gui/banner_editor/tabs/AbstractModifyPatternsTab.java
+++ src/main/java/fzmm/zailer/me/client/gui/banner_editor/tabs/AbstractModifyPatternsTab.java
@@ -3,7 +3,7 @@ package fzmm.zailer.me.client.gui.banner_editor.tabs;
 import fzmm.zailer.me.builders.BannerBuilder;
 import fzmm.zailer.me.client.gui.BaseFzmmScreen;
 import fzmm.zailer.me.client.gui.banner_editor.BannerEditorScreen;
-import io.wispforest.owo.ui.component.Components;
+import fzmm.zailer.me.client.gui.components.style.StyledComponents;
 import io.wispforest.owo.ui.component.ItemComponent;
 import io.wispforest.owo.ui.container.FlowLayout;
 import io.wispforest.owo.ui.core.Component;
@@ -49,7 +49,7 @@ public abstract class AbstractModifyPatternsTab implements IBannerEditorTab {
         for (var layer : layers) {
             builder.addLayer(layer);
 
-            ItemComponent itemComponent = Components.item(builder.copy().get());
+            ItemComponent itemComponent = StyledComponents.item(builder.copy().get());
             itemComponent.sizing(Sizing.fixed(32), Sizing.fixed(32));
 
             this.onItemComponentCreated(parent, itemComponent, layer, currentBanner, color);
