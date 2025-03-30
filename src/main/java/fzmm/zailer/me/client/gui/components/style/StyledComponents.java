47dafb6692ee595c318f6a962ac37ca0d84f04a1
Zailer43 <zailer.g2jku@slmails.com>
2025-03-30 20:52:08 -0300

    Add lazy-load to item tooltips to improve performance when loading categories in the Head Gallery * Generating a tooltip from a stack can be expensive, which is noticeable in the Head Gallery, change this so that it is only generated when hovering the cursor over it

--- src/main/java/fzmm/zailer/me/client/gui/components/style/StyledComponents.java
+++ src/main/java/fzmm/zailer/me/client/gui/components/style/StyledComponents.java
@@ -1,10 +1,33 @@
 package fzmm.zailer.me.client.gui.components.style;
 
+import fzmm.zailer.me.client.gui.components.style.component.StyledItemComponent;
 import fzmm.zailer.me.client.gui.components.style.component.StyledLabelComponent;
+import fzmm.zailer.me.utils.ItemUtils;
+import io.wispforest.owo.ui.core.CursorStyle;
+import io.wispforest.owo.ui.util.UISounds;
+import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 
 public class StyledComponents {
     public static StyledLabelComponent label(Text text) {
         return new StyledLabelComponent(text);
+    }
+
+    public static StyledItemComponent item(ItemStack stack) {
+        return new StyledItemComponent(stack);
+    }
+
+    public static StyledItemComponent itemGive(ItemStack stack) {
+        StyledItemComponent result = new StyledItemComponent(stack);
+        result.showOverlay(true)
+                .setTooltipFromStack(true)
+                .cursorStyle(CursorStyle.HAND)
+                .mouseDown().subscribe((mouseX, mouseY, button) -> {
+                    ItemUtils.give(stack);
+                    UISounds.playButtonSound();
+                    return true;
+                });
+
+        return result;
     }
 }
