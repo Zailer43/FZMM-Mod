0536dad8238fa147cb0ba8a509ad828793395c23
Zailer43 <zailer.g2jku@slmails.com>
2025-04-10 17:54:39 -0300

    Fix JVM Crash if head generator is closed in update previews * This could be easily reproduced if you press esc when opening it

--- src/main/java/fzmm/zailer/me/client/gui/head_generator/components/AbstractHeadComponentEntry.java
+++ src/main/java/fzmm/zailer/me/client/gui/head_generator/components/AbstractHeadComponentEntry.java
@@ -24,6 +24,7 @@ import net.minecraft.client.texture.NativeImage;
 import net.minecraft.client.texture.NativeImageBackedTexture;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
 
 import java.awt.image.BufferedImage;
 
@@ -32,7 +33,8 @@ public abstract class AbstractHeadComponentEntry extends StyledFlowLayout implem
     public static final int BODY_PREVIEW_SIZE = 12;
     protected final HeadGeneratorScreen parentScreen;
     private final NativeImageBackedTexture previewTexture;
-    private final Identifier textureId;
+    @Nullable
+    private Identifier textureId;
     protected AbstractHeadEntry entry;
     private EntityComponent<Entity> previewComponent;
     protected OverlayContainer<FlowLayout> overlayContainer;
@@ -74,6 +76,8 @@ public abstract class AbstractHeadComponentEntry extends StyledFlowLayout implem
     }
 
     public void setBodyPreview(boolean isBody) {
+        if (this.textureId == null)
+            return;
         this.isBodyPreview = isBody;
         Entity previewEntity;
         int size;
@@ -116,6 +120,8 @@ public abstract class AbstractHeadComponentEntry extends StyledFlowLayout implem
      * @param previewSkin Update preview with {@link BufferedImage}
      */
     public void updatePreview(BufferedImage previewSkin) {
+        if (this.textureId == null)
+            return;
         NativeImage nativeImage = ImageUtils.toNativeImage(previewSkin);
         this.previewTexture.setImage(nativeImage);
         this.previewTexture.upload();
@@ -133,6 +139,7 @@ public abstract class AbstractHeadComponentEntry extends StyledFlowLayout implem
 
     public void close() {
         MinecraftClient.getInstance().getTextureManager().destroyTexture(this.textureId);
+        this.textureId = null;
     }
 
     @Override
