3a5abfd6f5aa7220ac99970859bab159baeca900
Zailer43 <zailer.g2jku@slmails.com>
2025-12-29 20:19:46 -0300

    Add advanced search components * Add text filters to perform advanced searches * Each filter has a key and a value, an example usage is `id_greater=100` or `word=apple` * Add filters for String, Int, Date * Add an overlay to use filters more conveniently (and to see which ones exist) * Allow excluding in a filter with a `-` at the start, for example: `-word=apple`

---
+++ src/main/java/fzmm/zailer/me/client/gui/utils/text_filter/TextFilterDate.java
@@ -0,0 +1,40 @@
+package fzmm.zailer.me.client.gui.utils.text_filter;
+
+import io.wispforest.owo.config.ui.component.ConfigTextBox;
+import io.wispforest.owo.ui.component.TextBoxComponent;
+import net.minecraft.text.Text;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Optional;
+import java.util.function.BiPredicate;
+
+public class TextFilterDate<O> extends AbstractTextFilter<LocalDate, O> {
+    private final String pattern;
+    private final DateTimeFormatter formatter;
+
+    public TextFilterDate(String key, BiPredicate<LocalDate, O> predicate, String pattern) {
+        super(key, predicate);
+        this.pattern = pattern;
+        this.formatter = DateTimeFormatter.ofPattern(pattern);
+    }
+
+    @Override
+    protected Optional<LocalDate> parseValue(String text) {
+        try {
+            return Optional.of(LocalDate.parse(text, this.formatter));
+        } catch (DateTimeParseException ignored) {
+            return Optional.empty();
+        }
+    }
+
+    @SuppressWarnings("UnstableApiUsage")
+    @Override
+    public TextBoxComponent toInputComponent() {
+        ConfigTextBox result = new ConfigTextBox();
+        result.applyPredicate(s -> this.parseValue(s).isPresent());
+        result.setPlaceholder(Text.of(this.pattern));
+        return result;
+    }
+}
